a3c63daae983764f86ded1430a07776b777a2a5d
SilverCreekSc <austin.mcleod@silvercreeksoftware.com>
2020-03-13 14:14:01 -0600

    update download_url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Silver-Creek/pynomial.git",
-    download_url="",
+    download_url="https://github.com/Silver-Creek/pynomial/archive/v0.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
