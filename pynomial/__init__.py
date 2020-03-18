03f60b112f5c13ec7a2be6ee78371225803d4f5e
MHadavand <hadavand@ualberta.ca>
2020-03-18 13:40:37 -0600

    Added the global config class/dict. Modifie dthe read me to be consistent with PyPi

---
+++ pynomial/__init__.py
@@ -0,0 +1,13 @@
+'''
+Pynomial
+==================================
+
+
+
+'''
+from __future__ import absolute_import, division, print_function
+
+__version__ = '1.0.0'
+
+# parameter configurations
+from . pynomial_configuration import Configurations
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import setuptools
+import os
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
+
+
+with open ('requirements.txt', 'r') as file:
+    install_requires = file.readlines()
+install_requires = [x.strip() for x in install_requires]
 
 setuptools.setup(
     name="pynomial",
     version="0.1",
     author="Silver Creek",
-    author_email="austin.mcleod@silvercreeksoftware.com",
+    author_email="austin.mcleod@silvercreeksoftware.com; mostafa.hadavand@silvercreeksoftware.com",
     description="pynomial",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/markup",
     url="https://github.com/Silver-Creek/pynomial.git",
     download_url="https://github.com/Silver-Creek/pynomial/archive/v0.1.tar.gz",
     packages=setuptools.find_packages(),
@@ -19,5 +25,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
+    install_requires = install_requires,
+    zip_safe=False
 )
